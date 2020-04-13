637188545613d6498b47e77d726dd1cf73d582cf
Kirill <kirillkulakow31@gmail.com>
2020-04-13 10:15:02 +0300

    Updates

---
+++ precache-manifest.be2aef986a5e6474e9719af4df6dce61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e5c9784bd5a25f833d108363fbd677",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "c493511aadb43c3f2255",
+    "url": "/goit-react-hw-02-feedback/static/css/main.867c731a.chunk.css"
+  },
+  {
+    "revision": "9961bf3fa40e1c33135a",
+    "url": "/goit-react-hw-02-feedback/static/js/2.f8f12f61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-feedback/static/js/2.f8f12f61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c493511aadb43c3f2255",
+    "url": "/goit-react-hw-02-feedback/static/js/main.8d3ac395.chunk.js"
+  },
+  {
+    "revision": "1a2af30d5f9b5771679f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.a031b346.js"
+  }
+]);
